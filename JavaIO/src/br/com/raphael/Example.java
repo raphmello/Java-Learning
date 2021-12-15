a7e9235cd6aa7b1e12994c136ef4ce03cb83a15b
Raphael de Mello - RDM <raphael.mello@iteris.com.br>
2021-12-15 20:34:26 -0300

    JavaIO - Introdução a Exceptions

---
+++ JavaIO/src/br/com/raphael/Example.java
@@ -0,0 +1,45 @@
+package br.com.raphael;
+
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+public class Example {
+    public static void main(String[] args) {
+        try {
+            int result = divide();
+            System.out.println(result);
+        } catch (ArithmeticException | NoSuchElementException e) {
+            System.out.println(e.toString());
+            System.out.println("Unable to perform division, autopilot shuting down");
+        }
+    }
+
+    private static int divide() {
+        int x, y;
+//        try {
+            x = getInt();
+            y = getInt();
+            System.out.println("x is " + x + ", y is " + y);
+            return x/y;
+//        } catch (NoSuchElementException e) {
+//            throw new NoSuchElementException("no suitable input");
+//        } catch (ArithmeticException e) {
+//            throw new ArithmeticException("attempt to divide by zero");
+//        }
+    }
+
+    private static int getInt() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter an integer ");
+        while (true) {
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                // go round again
+                scanner.nextLine();
+                System.out.println("Please enter a number using only digits ");
+            }
+        }
+    }
+}
