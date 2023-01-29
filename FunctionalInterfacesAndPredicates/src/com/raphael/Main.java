1489eb193883d7e90f05a579b2cc83be607fd937
Raphael de Mello - RDM <raph.mello@gmail.com>
2023-01-29 18:44:12 -0300

    Functions

--- FunctionalInterfacesAndPredicates/src/com/raphael/Main.java
+++ FunctionalInterfacesAndPredicates/src/com/raphael/Main.java
@@ -3,19 +3,17 @@ package com.raphael;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Main {
 
     public static void main(String[] args) {
         Employee john = new Employee("John Doe", 30);
-        Employee tim = new Employee("Tim", 21);
-        Employee jack = new Employee("Jack", 40);
-        Employee snow = new Employee("Snow", 22);
-        Employee red = new Employee("Red", 32);
-        Employee charming = new Employee("Charming", 52);
+        Employee tim = new Employee("Tim Buch", 21);
+        Employee jack = new Employee("Jack Will", 40);
+        Employee snow = new Employee("Snow Flake", 22);
+        Employee red = new Employee("Red Wine", 32);
+        Employee charming = new Employee("Charming Whether", 52);
 
         List<Employee> employees = new ArrayList<>();
         employees.add(john);
@@ -40,7 +38,31 @@ public class Main {
         for (int i = 0; i < 10; i++) {
             System.out.println(randomSupplier.get());
         }
+//        employees.forEach(employee -> {
+//            String lastName = employee.getName().substring(employee.getName().indexOf(" ") + 1);
+//            System.out.println(lastName);
+//        });
 
+        employees.stream()
+                .map(getEmployeeLastNameFunction())
+                .forEach(System.out::println);
+
+        Function<Employee, String> upperCase = employee -> employee.getName().toUpperCase();
+        Function<String, String> firstName = name -> name.substring(0, name.indexOf(' '));
+        Function<Employee, String> chainedFunction = upperCase.andThen(firstName);
+        System.out.println(chainedFunction.apply(employees.get(0)));
+
+        BiFunction<String, Employee, String> concatAge = (name, employee) -> name.concat(" " + employee.getAge());
+        String upperName = upperCase.apply(employees.get(0));
+        System.out.println(concatAge.apply(upperName, employees.get(0)));
+
+        IntUnaryOperator incBy5 = i -> i + 5;
+        System.out.println(incBy5.applyAsInt(10));
+
+    }
+
+    private static Function<Employee, String> getEmployeeLastNameFunction() {
+        return employee -> employee.getName().substring(employee.getName().indexOf(" ") + 1);
     }
 
     public static void printEmployeesByAge(List<Employee> employees, String ageText, Predicate<Employee> ageCondition) {
